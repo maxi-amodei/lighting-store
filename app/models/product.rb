d366fee9e8ae4579ca20cbe8ae0c6dccfab610d5
maximiliano amodei <maxi_amodei@hotmail.com>
2025-04-18 13:00:32 -0300

    tailwind config

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   belongs_to :category
   has_many :product_tags
-has_many :tags, through: :product_tags
+  has_many :tags, through: :product_tags
 end
