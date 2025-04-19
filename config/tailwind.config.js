a9d7500a6ea2a80d7236ce3ab9067588a59b3948
maximiliano amodei <maxi_amodei@hotmail.com>
2025-04-19 17:34:54 -0300

    tailwind config add proftshaft gem

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,12 +1,48 @@
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
+    "./app/models/**/**/*.rb",
+    "./app/models/**/*.rb",
+    "./app/models/*.rb",
+    "./app/components/*.rb",
+    "./app/components/**/*.rb",
+    "./app/components/*.erb",
+    "./app/components/**/*.erb",
   ],
   theme: {
-    extend: {},
+    data: {
+      active: 'ui~="active"',
+    },
+    extend: {
+      blur: {
+        xs: "2px",
+      },
+      backdropBlur: {
+        xs: "2px",
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
+      },
+      fontSize: {
+        "2xs": ["0.625rem", "0.9rem"],
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in-out",
+        fadeOut: "fadeOut 0.5s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
+      },
+    },
   },
-  plugins: []
-} 
+};
