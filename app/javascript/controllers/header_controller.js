f084d40619f957dce280302cd549a79ebd480f84
maximiliano amodei <maxi_amodei@hotmail.com>
2025-04-19 18:35:11 -0300

    add controller

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,14 @@
+// app/javascript/controllers/header_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu", "search"]
+
+  toggleMenu() {
+    this.menuTarget.classList.toggle("hidden")
+  }
+
+  toggleSearch() {
+    this.searchTarget.classList.toggle("hidden")
+  }
+}
